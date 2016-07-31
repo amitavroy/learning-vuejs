b0133045b01f38a552457652dd4c61942e5d47e8
Amitav Roy <reachme@amitavroy.com>
2016-07-31 06:49:20 +0530

    First tutorial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,44 +2,52 @@
 <html>
     <head>
         <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <link rel="stylesheet" type="text/css" href="{{ url('css/app.css') }}">
     </head>
     <body>
         <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+            <h1>Welcome to Vuejs</h1>
+
+            <div class="container" id="vue-app">
+              <div class="row">
+                <div class="col-sm-12">
+                  <h1>My Todos</h1>
+                  <todo-item :todos="todos"></todo-item>
+                  
+                  <todo-add-form :newtodo.sync="newTodo"></todo-add-form>
+                </div>
+              </div>
+              
+              <template id="todo-list-template">
+                <div>
+                  <ul class="list-group">
+                    <li 
+                      v-for="todo in todos"
+                      class="list-group-item"
+                      v-bind:class="{ 'completed' : todo.completed }">
+                        @{{todo.title}}
+                        <button class="btn btn-xs pull-right margin-right-10" 
+                        v-bind:class="{'btn-success' : todo.completed, 'btn-danger' : !todo.completed}"
+                        v-on:click="todoCompleted(todo)">@{{todo.completed ? 'Completed' : 'Pending'}}</button>
+                      </li>
+                  </ul>
+                </div>
+              </template>
+              
+              <template id="todo-add-form">
+                <form v-on:submit.prevent="addTodo()">
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Enter new Todo" v-model="todo.title">
+                  </div>
+                  
+                  <div class="form-group">
+                    <button class="btn btn-success">Add</button>
+                  </div>
+                </form>
+              </template>
             </div>
         </div>
+
+        <script src="{{ elixir('js/main.js') }}"></script>
     </body>
 </html>
