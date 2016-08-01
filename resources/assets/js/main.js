6938fc713390fb8408d742ccc862cb31efb23f2a
Amitav Roy <reachme@amitavroy.com>
2016-08-01 09:04:53 +0530

    Some issues with gitignore. 

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,54 +1,26 @@
 import Vue from 'vue';
 
-var TodoItems = Vue.extend({
-    template: '#todo-list-template',
-  
-  props: ['todos'],
-  
-  methods: {
-    todoCompleted(todo) {
-        todo.completed = !todo.completed
-    }
-  }
-});
-
-var TodoAddForm = Vue.extend({
-    template: '#todo-add-form',
-  
-  props: ['newtodo'],
-  
-  data() {
-    return {
-        todo: {id: null, title: '', completed: false}
-    }
-  },
-  
-  methods: {
-    addTodo() {
-        this.newtodo = this.todo;
-      this.todo = {id: null, title: '', completed: false};
-    }
-  }
-});
+import TodoItems from './components/Todo/TodoItems.vue';
+import TodoAddForm from './components/Todo/TodoAdd.vue';
 
 Vue.component('todo-item', TodoItems);
 Vue.component('todo-add-form', TodoAddForm);
 
 new Vue({
     el: '#vue-app',
-  
-  data: {
-    todos: [{id: 1, title: 'Go Shopping', completed: true}],
-    newTodo: {}
-  },
-  
-  watch: {
-    newTodo(newval, oldval) {
-        this.todos.push({
-        id: Math.floor(Date.now()),
-        title: newval.title,
-        completed: false
-      });
+
+    data: {
+        todos: [{id: 1, title: 'Go Shopping', completed: true}],
+        newTodo: {}
+    },
+
+    watch: {
+        newTodo(newval, oldval) {
+            this.todos.push({
+                id: Math.floor(Date.now()),
+                title: newval.title,
+                completed: false
+            });
+        }
     }
-  }
 });
