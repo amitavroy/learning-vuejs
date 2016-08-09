020f4f7bd61f3f9438190bf7559777101c447046
Amitav Roy <reachme@amitavroy.com>
2016-08-09 22:33:44 +0530

    Some basic styling and added Font awesome library

--- resources/assets/js/components/Todo/TodoAdd.vue
+++ resources/assets/js/components/Todo/TodoAdd.vue
@@ -20,11 +20,13 @@
 <template>
     <form v-on:submit.prevent="addTodo()">
       <div class="form-group">
-        <input type="text" class="form-control" placeholder="Enter new Todo" v-model="todo.title">
+        <input type="text" class="form-control" placeholder="Add a new Todo" v-model="todo.title">
       </div>
       
       <div class="form-group">
-        <button class="btn btn-success">Add</button>
+        <button class="btn btn-primary">
+            <i class="fa fa-save"></i> Save
+        </button>
       </div>
     </form>
 </template>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,36 +3,44 @@
     <head>
         <title>Laravel</title>
         <link rel="stylesheet" type="text/css" href="{{ url('css/app.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ url('plugins/fa/css/font-awesome.min.css') }}">
     </head>
     <body>
-        <div class="container">
-            <h1>Welcome to Vuejs</h1>
-
-            <div class="container" id="vue-app">
-              <div class="row">
+        <div class="container" id="vue-app">
+            <div class="row">
                 <div class="col-sm-12">
-                  <h1>My Todos</h1>
-
-                  <todo-item :todos="todos"></todo-item>
-                  
-                  <todo-add-form :newtodo.sync="newTodo"></todo-add-form>
+                    @include('partials/top-menu')
+                    <h1>Welcome to Vue.js</h1>
                 </div>
-              </div>
-              
-              <template id="todo-add-form">
-                <form v-on:submit.prevent="addTodo()">
-                  <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Enter new Todo" v-model="todo.title">
-                  </div>
-                  
-                  <div class="form-group">
-                    <button class="btn btn-success">Add</button>
-                  </div>
-                </form>
-              </template>
+            </div>
+            <div class="row">
+                
+                <div class="col-sm-7">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">My Todos</div>
+                        <div class="panel-body">
+                            {{-- Body content start --}}
+                            <todo-item :todos="todos"></todo-item>
+                            <todo-add-form :newtodo.sync="newTodo"></todo-add-form>
+                            {{-- Body content end --}}
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-5">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">My Todos</div>
+                        <div class="panel-body">
+                            <p>This is the sidebar where something else will come.</p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
-
+        </div>
+        <script src="{{ url('plugins/jquery-2.2.4.min.js') }}"></script>
+        <script src="{{ url('plugins/bootstrap.min.js') }}"></script>
         <script src="{{ elixir('js/main.js') }}"></script>
     </body>
 </html>
