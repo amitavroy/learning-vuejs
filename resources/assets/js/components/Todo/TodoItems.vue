dc1d22b27963cc89983afb9d398422d5e19e4acc
Amitav Roy <reachme@amitavroy.com>
2016-08-10 06:41:53 +0530

    Added the delete functionality.

--- resources/assets/js/components/Todo/TodoItems.vue
+++ resources/assets/js/components/Todo/TodoItems.vue
@@ -6,6 +6,10 @@
         methods: {
             todoCompleted(todo) {
                 todo.completed = !todo.completed
+            },
+
+            todoDelete(todo) {
+                this.todos.$remove(todo);
             }
         }
     }
@@ -19,8 +23,14 @@
           class="list-group-item"
           v-bind:class="{ 'completed' : todo.completed }">
             {{todo.title}}
+
+            <button 
+                class="btn btn-xs btn-danger pull-right"
+                v-on:click="todoDelete(todo)">
+                <i class="fa fa-trash"></i>
+            </button>
             <button class="btn btn-xs pull-right margin-right-10" 
-            v-bind:class="{'btn-success' : todo.completed, 'btn-danger' : !todo.completed}"
+            v-bind:class="{'btn-success' : todo.completed, 'btn-warning' : !todo.completed}"
             v-on:click="todoCompleted(todo)">{{todo.completed ? 'Completed' : 'Pending'}}</button>
           </li>
       </ul>
